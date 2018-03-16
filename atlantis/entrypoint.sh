8f3dba2ddf15a39e428ed22fe798cd0b617058cd
gappan <azhagappan.sivaraman@razorpay.com>
2018-03-16 16:28:12 +0530

    atlantis: dockerfile for atlantis server

---
+++ atlantis/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+set -e
+
+#Alohomora
+ALOHOMORA_BIN=$(which alohomora)
+
+echo "casting alohomora - vault"
+$ALOHOMORA_BIN cast --region ap-south-1 --env prod --app ops "/atlantis/config.yaml.j2"
+
+#Starting atlantis server
+atlantis server --config="/atlantis/config.yaml" --require-approval --repo-whitelist 'github.com/razorpay/vishnu'
+
