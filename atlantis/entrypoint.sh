545f42cba03fcfc6ff7f8e2ccc893e0ca5d3d433
Nemo <me@captnemo.in>
2018-04-27 12:50:41 +0530

    More cleanup

--- atlantis/entrypoint.sh
+++ atlantis/entrypoint.sh
@@ -2,11 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-#Alohomora
-ALOHOMORA_BIN=$(which alohomora)
-
 echo "casting alohomora - vault"
-$ALOHOMORA_BIN cast --region ap-south-1 --env prod --app ops "/atlantis/config.yaml.j2"
+alohomora cast --region ap-south-1 --env prod --app ops "/atlantis/config.yaml.j2"
 
 # Starting atlantis server
 atlantis server --config="/atlantis/config.yaml" --require-approval --repo-whitelist 'github.com/razorpay/vishnu'
--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,20 +1,12 @@
-#!/usr/bin/env sh
-set -e
-
-#Alohomora
-ALOHOMORA_BIN=$(which alohomora)
+#!/usr/bin/dumb-init /bin/sh
+set -euo pipefail
+IFS=$'\n\t'
 
 echo "casting alohomora - vault"
-$ALOHOMORA_BIN cast --region ap-south-1 --env prod --app ops "/app/conf/atlantis.cfg.j2"
-$ALOHOMORA_BIN cast --region ap-south-1 --env prod --app ops "/app/conf/google.json.j2"
-
-#Copying files
-cp /app/conf/atlantis.conf /etc/nginx/conf.d/atlantis.conf
-cp /app/conf/atlantis.cfg /etc/oauth2proxy/atlantis.cfg
-cp /app/conf/google.json /etc/oauth2proxy/google.json
+$ALOHOMORA_BIN cast --region ap-south-1 --env prod --app ops "/app/conf/atlantis.cfg.j2" "/app/conf/google.json.j2"
 
 #Starting oauth2proxy
-oauth2_proxy --config=/etc/oauth2proxy/atlantis.cfg &
+oauth2_proxy --config=/app/conf/atlantis.cfg &
 
 #Starting nginx
 mkdir /run/nginx
